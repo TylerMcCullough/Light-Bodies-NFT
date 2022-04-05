4756a16c16654c7ccb23258fa2be489d23c8926d
HarshitMeghpara <harshitmeghpara@icloud.com>
2022-04-05 16:13:00 +0530

    sell Function Updates

--- scripts/sellNFT.js
+++ scripts/sellNFT.js
@@ -2,38 +2,35 @@ require("dotenv").config()
 const OPENSEA_KEY = process.env.OPENSEA_KEY
 const ALCHEMY_KEY = process.env.ALCHEMY_KEY
 const contractAddress = process.env.CONTRACT_ADDRESS
-const NETWORK = process.env.NETWORK;
+const NETWORK = process.env.NETWORK
 const OWNER_ADDRESS = process.env.PUBLIC_KEY
-const Web3 = require("web3");
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const INFURA_KEY = process.env.INFURA_KEY
 
 
 const os = require("opensea-js")
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const provider = new HDWalletProvider({
-  mnemonic: "wallet mnemonic",
-  providerOrUrl: ALCHEMY_KEY,
-  addressIndex: 1
-});
+const provider = new HDWalletProvider(PRIVATE_KEY, INFURA_KEY)
+
 const seaport = new os.OpenSeaPort(
     provider,
     {
       networkName:
         NETWORK === "mainnet" || NETWORK === "live"
-          ? os.Network.Matic
+          ? os.Network.Main
           : os.Network.Mumbai,
       apiKey: OPENSEA_KEY,
     }
   );
   
-const sellNFT = async (tokenID, price) => {
+const sellNFT = async (tokenID) => {
     console.log("Auctioning an item for a fixed price...");
     const fixedPriceSellOrder = await seaport.createSellOrder({
       asset: {
         tokenId: tokenID,
         tokenAddress: contractAddress,
       },
-      startAmount: price,
-      endAmount: price,
+      startAmount: 0.08,
       expirationTime: 0,
       accountAddress: OWNER_ADDRESS,
     });
@@ -43,4 +40,5 @@ const sellNFT = async (tokenID, price) => {
     return fixedPriceSellOrder.asset.openseaLink
     }
 
+sellNFT(1)
 module.exports = sellNFT
